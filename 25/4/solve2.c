5602b85970ee9fa40382c861f7cadbe6b6267965
Matthew Hogg <gg0h.ctf@gmail.com>
2025-12-04 20:00:31 +0000

    25 day 4

---
+++ 25/4/solve2.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define IN_SIZE 140
+#define ARRAY_SIZE IN_SIZE + 1
+
+
+int main() {
+
+    char grid[IN_SIZE][ARRAY_SIZE];
+    for(int i =0; i < IN_SIZE; ++i) {
+        scanf("%s", grid[i]);
+        //printf("%s\n", grid[i]);  
+    }
+    int left = 1, right = 1, above = 1, below = 1, count = 0, loop_found = 0, found = 0;
+    do {
+        loop_found = 0;
+        for(int i = 0; i < IN_SIZE; i++){
+            for(int j =0; j< ARRAY_SIZE; j++) {
+                
+                if (grid[i][j] == '@') {
+                                // turn off check flags for perimiter
+                    if (i == 0) {
+                        above = 0;
+                    }
+                    if (j == 0) {
+                        left = 0;
+                    }
+                    if (i == IN_SIZE - 1) {
+                        below = 0;
+                    }
+                    if (j == IN_SIZE) {
+                        right = 0;
+                    }
+                    
+                    //printf("i %d j %d\n", i, j);
+                    if (above) {
+                        if (grid[i-1][j] == '@') count++;
+                        if (right) {
+                            if (grid[i-1][j+1] == '@') count++;
+                        }
+
+                        if (left) {
+                            if (grid[i-1][j-1] == '@') count++;
+                        }
+
+                    }
+
+                    if (right) {
+                        if (grid[i][j+1] == '@') count++;
+                    }
+
+                    if (left) {
+                        if (grid[i][j-1] == '@') count++;
+                    }
+
+                    if (below) {
+                        if (grid[i+1][j] == '@') count++;
+                        if (right) {
+                            if (grid[i+1][j+1] == '@') count++;
+                        }
+
+                        if (left) {
+                            if (grid[i+1][j-1] == '@') count++;
+                        }
+
+                    }
+
+                    if(count < 4) {
+                        loop_found++;
+                        grid[i][j] = 'x';
+                    }
+
+                    left = 1, right = 1, above = 1, below = 1, count = 0;
+                }
+                
+
+            }
+        }
+        printf("loop found: %d\n", loop_found);
+        printf("=========\n");
+        found += loop_found;
+    } while (loop_found);
+    printf("found: %d\n", found);
+
+
+}
+/**
+
+
+*/
